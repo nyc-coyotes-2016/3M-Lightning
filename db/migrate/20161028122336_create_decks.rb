797087467746e92124062d34a28d37a944e9cbe3
Marissa Traina <marissatraina@gmail.com>
2016-10-28 12:38:53 -0400

    Add keys to table migrations

--- db/migrate/20161028122336_create_decks.rb
+++ db/migrate/20161028122336_create_decks.rb
@@ -1,4 +1,10 @@
 class CreateDecks < ActiveRecord::Migration
   def change
+    create_table :decks do |t|
+      t.string :subject, { null: false }
+      t.integer :user_id
+
+      t.timestamps(null: false)
+    end
   end
 end
--- db/migrate/20161028122346_create_guesses.rb
+++ db/migrate/20161028122346_create_guesses.rb
@@ -1,4 +1,10 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
+    create_table :guesses do |t|
+      t.integer :round_id, { null: false }
+      t.integer :card_id, { null: false }
+
+      t.timestamps(null: false)
+    end
   end
 end
